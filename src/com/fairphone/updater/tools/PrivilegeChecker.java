9bb9b6c151426dd9d01c14abba4ac3cc7293fa1e
Jose Pascoal <jose.pascoal@kwamecorp.com>
2015-02-13 18:16:24 +0000

    - Bump version number - Add logs to PrivilegeChecker

--- src/com/fairphone/updater/tools/PrivilegeChecker.java
+++ src/com/fairphone/updater/tools/PrivilegeChecker.java
@@ -1,15 +1,21 @@
 package com.fairphone.updater.tools;
 
 
+import android.util.Log;
+
 import java.io.File;
 
 public final class PrivilegeChecker {
+    private static final String TAG = PrivilegeChecker.class.getSimpleName();
+
 	private static final boolean isPrivilegedApp;
 
 	static {
 		// If we have permissions to write instructions to the recovery, we are a privileged app.
 		File command = new File("/cache/recovery/command");
+        File command2 = new File("/cache/command");
 		isPrivilegedApp = command.canWrite();
+        Log.wtf(TAG, "comand: " + command.canWrite() + "\ncommand2: "+ command2.canWrite());
 	}
 
 	public static boolean isPrivilegedApp(){
